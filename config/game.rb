5beb582d55348535d2ce1247c0d7efc4fc3676ad
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-03-06 01:02:28 +0800

    Modify game.rb in order to view the bar data

--- config/game.rb
+++ config/game.rb
@@ -13,7 +13,7 @@ module NewGame
 		$CHARACTER = Character.new(gets.chomp, 20, 5, 0, 5, 2, 3)
 		$CHARACTER_MAX_HP = 20
 		$CHARACTER_MAX_MP = 5
-		$CHARACTER_MAX_EXP = 0
+		$CHARACTER_MAX_EXP = 10
 		$CHARACTER_ATTACK = 5
 		$CHARACTER_DEFENSE = 2
 		$CHARACTER_AGILITY = 3
@@ -56,9 +56,41 @@ module ProcessGame
 
 	def show_current_state
 		puts "\t<<< #{$CHARACTER.name.light_yellow} >>>"
-		puts "\tHP  | #{$CHARACTER.hp.to_s.light_red}"
-		puts "\tMP  | #{$CHARACTER.mp.to_s.blue}"
-		puts "\tEXP | #{$CHARACTER.exp.to_s.light_green}"
+
+		hp_display_length = $CHARACTER.hp.to_s.length + 1 + $CHARACTER_MAX_HP.to_s.length
+		mp_display_length = $CHARACTER.mp.to_s.length + 1 + $CHARACTER_MAX_MP.to_s.length
+		attack_display_length = $CHARACTER_ATTACK.to_s.length
+		defense_display_length = $CHARACTER_DEFENSE.to_s.length
+		agility_display_length = $CHARACTER_AGILITY.to_s.length
+		exp_display_length = $CHARACTER.exp.to_s.length + 1 + $CHARACTER_MAX_EXP.to_s.length
+
+		hp_space_display_length = 40 - hp_display_length
+		mp_space_display_length = 40 - mp_display_length
+		attack_space_display_length = 40 - attack_display_length
+		defense_space_display_length = 40 - defense_display_length
+		agility_space_display_length = 40 - agility_display_length
+		exp_space_display_length = 40 - exp_display_length
+
+		bar_length = 48
+		hp_bar  = ( Rational($CHARACTER.hp,  $CHARACTER_MAX_HP ) * bar_length ).to_i
+		mp_bar  = ( Rational($CHARACTER.mp,  $CHARACTER_MAX_MP ) * bar_length ).to_i
+		exp_bar = ( Rational($CHARACTER.exp, $CHARACTER_MAX_EXP) * bar_length ).to_i
+		hp_bar_space  = bar_length - hp_bar
+		mp_bar_space  = bar_length - mp_bar
+		exp_bar_space = bar_length - exp_bar
+
+		puts "\tHP       |" + " " * hp_space_display_length + "#{$CHARACTER_MAX_HP}/#{$CHARACTER.hp.to_s.light_red}"
+		puts "\t[" + "|".light_red * hp_bar + " " * hp_bar_space + "]"
+		
+		puts "\tMP       |" + " " * mp_space_display_length + "#{$CHARACTER_MAX_MP}/#{$CHARACTER.mp.to_s.blue}"
+		puts "\t[" + "|".blue * mp_bar + " " * mp_bar_space + "]"
+		
+		puts "\tEXP      |" + " " * exp_space_display_length + "#{$CHARACTER_MAX_EXP}/#{$CHARACTER.exp.to_s.light_green}"
+		puts "\t[" + "|".light_green * exp_bar + " " * exp_bar_space + "]" 
+
+		puts "\tATTACK   |" + " " * attack_space_display_length + "#{$CHARACTER_ATTACK.to_s.red}"
+		puts "\tDEFENSE  |" + " " * defense_space_display_length + "#{$CHARACTER_DEFENSE.to_s.light_cyan}"
+		puts "\tAGILITY  |" + " " * agility_space_display_length + "#{$CHARACTER_AGILITY.to_s.light_magenta}"
 	end
 
 end

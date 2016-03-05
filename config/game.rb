960143206d0ae71c496028dcfcf2abb8c10ea301
Huang Jun-Xin <destinydragon0614@yahoo.com.tw>
2016-03-06 01:30:12 +0800

    Refactor code

--- config/game.rb
+++ config/game.rb
@@ -1,4 +1,5 @@
 require_relative 'config_helper'
+require_relative 'game/game_helper'
 require_relative 'game/explore'
 require_relative 'game/character'
 require 'colorize'
@@ -30,8 +31,9 @@ module NewGame
 end
 
 module ProcessGame
+	include GameHelper
 	def process
-		loop do
+		while $PROCESS_GAME_TOKEN
 			striped_line
 			show_current_state
 			striped_line
@@ -46,51 +48,17 @@ module ProcessGame
 					include Explore
 					random_token_generator(1..100)
 					explore
-				when 2, 3, 4
+				when 2
+				when 3
+				when 4
 					puts "\tBye Bye ~!"
+					new_line
 					$PROCESS_GAME_TOKEN = false
 					$QUIT_GAME_TOKEN = true
 			end
 		end
 	end
-
-	def show_current_state
-		puts "\t<<< #{$CHARACTER.name.light_yellow} >>>"
-
-		hp_display_length = $CHARACTER.hp.to_s.length + 1 + $CHARACTER_MAX_HP.to_s.length
-		mp_display_length = $CHARACTER.mp.to_s.length + 1 + $CHARACTER_MAX_MP.to_s.length
-		attack_display_length = $CHARACTER_ATTACK.to_s.length
-		defense_display_length = $CHARACTER_DEFENSE.to_s.length
-		agility_display_length = $CHARACTER_AGILITY.to_s.length
-		exp_display_length = $CHARACTER.exp.to_s.length + 1 + $CHARACTER_MAX_EXP.to_s.length
-
-		hp_space_display_length = 40 - hp_display_length
-		mp_space_display_length = 40 - mp_display_length
-		attack_space_display_length = 40 - attack_display_length
-		defense_space_display_length = 40 - defense_display_length
-		agility_space_display_length = 40 - agility_display_length
-		exp_space_display_length = 40 - exp_display_length
-
-		bar_length = 48
-		hp_bar  = ( Rational($CHARACTER.hp,  $CHARACTER_MAX_HP ) * bar_length ).to_i
-		mp_bar  = ( Rational($CHARACTER.mp,  $CHARACTER_MAX_MP ) * bar_length ).to_i
-		exp_bar = ( Rational($CHARACTER.exp, $CHARACTER_MAX_EXP) * bar_length ).to_i
-		hp_bar_space  = bar_length - hp_bar
-		mp_bar_space  = bar_length - mp_bar
-		exp_bar_space = bar_length - exp_bar
-
-		puts "\tHP       |" + " " * hp_space_display_length + "#{$CHARACTER_MAX_HP}/#{$CHARACTER.hp.to_s.light_red}"
-		puts "\t[" + "|".light_red * hp_bar + " " * hp_bar_space + "]"
-		
-		puts "\tMP       |" + " " * mp_space_display_length + "#{$CHARACTER_MAX_MP}/#{$CHARACTER.mp.to_s.blue}"
-		puts "\t[" + "|".blue * mp_bar + " " * mp_bar_space + "]"
-		
-		puts "\tEXP      |" + " " * exp_space_display_length + "#{$CHARACTER_MAX_EXP}/#{$CHARACTER.exp.to_s.light_green}"
-		puts "\t[" + "|".light_green * exp_bar + " " * exp_bar_space + "]" 
-
-		puts "\tATTACK   |" + " " * attack_space_display_length + "#{$CHARACTER_ATTACK.to_s.red}"
-		puts "\tDEFENSE  |" + " " * defense_space_display_length + "#{$CHARACTER_DEFENSE.to_s.light_cyan}"
-		puts "\tAGILITY  |" + " " * agility_space_display_length + "#{$CHARACTER_AGILITY.to_s.light_magenta}"
-	end
-
 end
+
+
+
---
+++ config/game/game_helper.rb
@@ -0,0 +1,49 @@
+module GameHelper
+
+	def show_current_state
+		puts "\t<<< #{$CHARACTER.name.light_yellow} >>>"
+
+		hp_display_length = $CHARACTER.hp.to_s.length + 1 + $CHARACTER_MAX_HP.to_s.length
+		mp_display_length = $CHARACTER.mp.to_s.length + 1 + $CHARACTER_MAX_MP.to_s.length
+		attack_display_length = $CHARACTER_ATTACK.to_s.length
+		defense_display_length = $CHARACTER_DEFENSE.to_s.length
+		agility_display_length = $CHARACTER_AGILITY.to_s.length
+		exp_display_length = $CHARACTER.exp.to_s.length + 1 + $CHARACTER_MAX_EXP.to_s.length
+
+		hp_space_display_length = 40 - hp_display_length
+		mp_space_display_length = 40 - mp_display_length
+		attack_space_display_length = 40 - attack_display_length
+		defense_space_display_length = 40 - defense_display_length
+		agility_space_display_length = 40 - agility_display_length
+		exp_space_display_length = 40 - exp_display_length
+
+		bar_length = 48
+
+		puts "\tHP       |" + " " * hp_space_display_length + "#{$CHARACTER_MAX_HP}/#{$CHARACTER.hp.to_s.light_red}"
+		display_bar(bar_length, $CHARACTER.hp, $CHARACTER_MAX_HP, "light_red")
+
+		puts "\tMP       |" + " " * mp_space_display_length + "#{$CHARACTER_MAX_MP}/#{$CHARACTER.mp.to_s.blue}"
+		display_bar(bar_length, $CHARACTER.mp, $CHARACTER_MAX_MP, "blue")
+
+		puts "\tEXP      |" + " " * exp_space_display_length + "#{$CHARACTER_MAX_EXP}/#{$CHARACTER.exp.to_s.light_green}"
+		display_bar(bar_length, $CHARACTER.exp, $CHARACTER_MAX_EXP, "light_green")
+
+		puts "\tATTACK   |" + " " * attack_space_display_length + "#{$CHARACTER_ATTACK.to_s.red}"
+		puts "\tDEFENSE  |" + " " * defense_space_display_length + "#{$CHARACTER_DEFENSE.to_s.light_cyan}"
+		puts "\tAGILITY  |" + " " * agility_space_display_length + "#{$CHARACTER_AGILITY.to_s.light_magenta}"
+	end
+	
+	def display_bar(bar_length, numerator, denominator, color)
+		bar = ( Rational(numerator, denominator) * bar_length ).to_i
+		bar_space = bar_length - bar
+		case color
+			when "light_red"
+				puts "\t[" + "|".light_red * bar + " " * bar_space + "]"
+			when "blue"
+				puts "\t[" + "|".blue * bar + " " * bar_space + "]"		
+			when "light_green"
+				puts "\t[" + "|".light_green * bar + " " * bar_space + "]"
+		end
+	end
+
+end
